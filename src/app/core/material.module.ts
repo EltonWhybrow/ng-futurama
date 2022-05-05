931ab3bdb30352044a96b4142eb6ebc53d5dee40
Elton <eltonwhybrow@icloud.com>
2022-05-05 18:06:01 +0100

    material updates

--- src/app/core/material.module.ts
+++ src/app/core/material.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 const material = [
   // MatSliderModule
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatGridListModule,
+  MatCardModule
 ]
 
 
 @NgModule({
   imports: [
     CommonModule,
-    // material
+    material
   ],
   exports: [
-    // material
+    material
   ]
 })
 export class MaterialModule { }
